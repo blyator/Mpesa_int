4aefc397f4aa4c12676cbf550017dec7b78864cf
Billy Yator <dmnbilly@gmail.com>
2025-12-15 09:16:16 +0300

    Update

--- backend/mpesa/settings.py
+++ backend/mpesa/settings.py
@@ -27,15 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^opurli9y-c8np8vhixfw4c%es0t*@^!q)c6m8i0=v&aoxeyox'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'kyler-unwrung-umbrageously.ngrok-free.dev',
-    'mpesa-fsug.onrender.com'
-]
-
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
